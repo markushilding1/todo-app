238d0d68300e75b6bac66dacad1d95a4f437976a
Markus Hilding <markushilding@gmail.com>
2024-02-15 21:15:12 +0100

    Update listTodos response and argument types

--- src/services/todos.ts
+++ src/services/todos.ts
@@ -1,12 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+interface Todo {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
+}
+
 export const todosApi = createApi({
   reducerPath: 'todosApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://jsonplaceholder.typicode.com',
   }),
   endpoints: builder => ({
-    listTodos: builder.query({
+    listTodos: builder.query<Todo[], number | void>({
       query: () => 'todos/',
     }),
   }),
