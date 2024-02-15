c1e44f6f53e71de83b077b63616328f5ff22e964
Markus Hilding <markushilding@gmail.com>
2024-02-15 11:53:22 +0100

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'no-console': ['error', { allow: ['warn', 'error', 'debug'] }],
+  },
+};
