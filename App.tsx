755193b5ba6c831b91a4136bc58dfe93da079141
Markus Hilding <markushilding@gmail.com>
2024-02-18 19:45:38 +0100

    Add gesture handler

--- App.tsx
+++ App.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { store } from './src/store';
 import Navigation from './src/navigation';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <Provider store={store}>
-        <View style={styles.container}>
-          <Navigation />
-        </View>
-      </Provider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={styles.gestureHandlerRootView}>
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <Navigation />
+          </View>
+        </Provider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
@@ -22,5 +25,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexGrow: 1,
+  },
+  gestureHandlerRootView: {
+    flex: 1,
   },
 });
