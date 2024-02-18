ed218875420ed48cd6775238e5c4f28ccc595221
Markus Hilding <markushilding@gmail.com>
2024-02-18 13:58:33 +0100

    Add redux-logger middleware for redux debugging

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+
 import { todosApi } from './services/todos';
 
 export const store = configureStore({
   reducer: {
     [todosApi.reducerPath]: todosApi.reducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(todosApi.middleware),
+  middleware: getDefaultMiddleware => {
+    const defaultMiddleware = getDefaultMiddleware();
+    const middleWare = defaultMiddleware.concat(todosApi.middleware);
+    return __DEV__ ? middleWare.concat(createLogger()) : middleWare;
+  },
+  devTools: __DEV__,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
