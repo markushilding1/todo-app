88a059718a155fde34e83c688a8011255edf85f7
Markus Hilding <markushilding@gmail.com>
2024-02-18 14:31:17 +0100

    Implement updateTodo endpoint in todos api
    Patches a todo item server-side and optimistically updates the affected item in the "getTodos" cache.
    
    If the server-side api did in fact patch the item, "tags" would allow RTK Query to fetch the updated item alone and automatically update the affected caches.

--- src/services/todos.ts
+++ src/services/todos.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-interface Todo {
+export interface Todo {
   id: number;
   userId: number;
   title: string;
@@ -13,12 +13,34 @@ export const todosApi = createApi({
     baseUrl: 'https://jsonplaceholder.typicode.com',
   }),
   endpoints: builder => ({
-    listTodos: builder.query<Todo[], number | void>({
-      query: () => 'todos/',
+    getTodos: builder.query<Todo[], number | void>({
+      query: () => '/todos',
+    }),
+    updateTodo: builder.mutation<Todo, Partial<Todo>>({
+      query: ({ id, ...body }) => ({
+        url: `todos/${id}`,
+        method: 'PATCH',
+        body,
+      }),
+      async onQueryStarted({ id, ...body }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          todosApi.util.updateQueryData('getTodos', undefined, draftTodos =>
+            draftTodos.map(todo =>
+              todo.id === id ? { ...todo, ...body } : todo,
+            ),
+          ),
+        );
+
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useListTodosQuery } = todosApi;
+export const { useGetTodosQuery, useUpdateTodoMutation } = todosApi;
